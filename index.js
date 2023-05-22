44a7030ffe111ac7989f3c8d133633cebc2d2aed
DSmith575 <SMITDE5@student.op.ac.nz>
2023-05-22 22:20:59 +1200

    version change

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const { resolve } = require("path");
 
 try {
+console.log("Installing tidy-cz | prettier | commitizen")
+
   const prettierrcContent = `{
     "singleQuote": false,
     "semi": true,
@@ -19,7 +21,6 @@ try {
   fs.writeFileSync(prettierIgnorePath, prettierIgnoreContent, "utf8");
   console.log(".prettierignore file created.");
 
-  // Update package.json scripts and devDependencies
   const packageJsonPath = resolve(__dirname, "..", "..", "package.json");
   const packageJson = require(packageJsonPath);
 
